310cf0e2a169aaf22d96395a2d9af4f7076831ca
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-06-22 22:48:21 +0200

    Added Advanced Filtering, Select & Sorting to GET all Bootcamps

--- controlers/bootcamps.js
+++ controlers/bootcamps.js
@@ -7,7 +7,36 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  const reqQuery = { ...req.query };
+
+  const removeFields = ['select', 'sort'];
+
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  let queryStr = JSON.stringify(reqQuery);
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  const bootcamps = await query;
 
   res
     .status(200)
